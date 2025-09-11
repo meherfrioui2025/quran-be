13a781d58b70408cc3b936f9551334b0950edaf8
Frioui <frioui@MacBook-Air-de-Frioui.local>
2025-09-11 09:43:18 +0200

    add api

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,7 @@
 import express from "express";
+import { config } from "dotenv";
+config(); 
+
 import apiRoutes from "./api/index.js";
 import { app } from "../config/keys.js";
 
@@ -8,7 +11,7 @@ const api = `/${apiURL}`;
 const router = express.Router();
 
 // api routes
-router.get(apiURL,(req,res)=>"it is a live!")
+router.get(process.env.API_URL,(req,res)=>"it is a live!")
 router.use(api, apiRoutes);
 router.use(api, (req, res) => res.status(404).json("No API route found"));
 
