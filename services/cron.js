369331b44de0ceedb74e058e4009e9a2691e10be
Frioui <frioui@MacBook-Air-de-Frioui.local>
2025-09-11 11:03:37 +0200

    fix api

--- services/cron.js
+++ services/cron.js
@@ -1,15 +1,12 @@
 import cron from "cron";
 import https from "https";
 
-import { app } from "../config/keys.js";
 
-const { apiURL } = app;
 
-const api = `/${apiURL}`;
 
 const job = new cron.CronJob("*/14 * * * *", function () {
   https
-    .get(api, (res) => {
+    .get(process.env.API_URL, (res) => {
       if (res.statusCode === 200) console.log("GET request sent successfully");
       else console.log("GET request failed", res.statusCode);
     })
